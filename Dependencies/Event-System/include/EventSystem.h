36cfcfbe110a647e922c7d4a99dfda796da7a133
LanguidBasil <e.makunev@gmail.com>
2022-03-08 17:36:55 +1000

    Updated EventSystem

--- Dependencies/Event-System/include/EventSystem.h
+++ Dependencies/Event-System/include/EventSystem.h
@@ -2,21 +2,67 @@
 
 #include <vector>
 #include <functional>
+#include <type_traits>
 
 namespace EventSystem
 {
+	struct EventArgs {};
+
+	template <typename Args>
 	class Event
 	{
 	public:
-		explicit Event();
-		explicit Event(const Event&) = delete;
-		~Event();
+		explicit Event()
+			: Subscribed(std::vector<std::function<void(Args)>>())
+		{
+			static_assert(std::is_base_of<EventArgs, Args>::value, "Invalid type, Args have to be derived from EventArgs");
+		}
 
-		void Fire();
-		void operator += (std::function<void()>);
-		void operator -= (std::function<void()>);
+		explicit Event(const Event&) = delete;
+		~Event() {}
+
+		void Fire(const Args& args)
+		{
+			for (auto& func : Subscribed)
+				func(args);
+		}
+
+		void Add(std::function<void(Args)> func)
+		{
+			Subscribed.push_back(func);
+		}
+
+		void Remove(std::function<void(Args)> func)
+		{
+			for (auto it = Subscribed.begin(); it != Subscribed.end(); it++)
+			{
+				const auto& current = it->target<void()>();
+				const auto& target = func.target<void()>();
+
+				if (current == target)
+				{
+					Subscribed.erase(it);
+					return;
+				}
+			}
+		}
+
+		void RemoveAll()
+		{
+			Subscribed.clear();
+		}
+
+		void operator += (std::function<void(Args)> func)
+		{
+			Add(func);
+		}
+
+		void operator -= (std::function<void(Args)> func)
+		{
+			Remove(func);
+		}
 
 	private:
-		std::vector<std::function<void()>> Subscribed;
+		std::vector<std::function<void(Args)>> Subscribed;
 	};
 }
--- Minesweeper/Sources/main.cpp
+++ Minesweeper/Sources/main.cpp
@@ -1,18 +1,9 @@
 #include "Minesweeper/Minesweeper.h"
-#include "EventSystem.h"
-
-#include <iostream>
 
 int main()
 {
 	// TODO: Add timer
 	// TODO: Add winning
 	// TODO: Add difficulty
-	//Minesweeper::StartGame();
-
-	// TODO: Add events
-	EventSystem::Event e;
-
-	e += []() { std::cout << "Hello World!\n"; };
-	e.Fire();
+	Minesweeper::StartGame();
 }
